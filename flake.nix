6b8493708b56b78255f5368c8a17befa10c3f3a0
Ethan-Winer <ethan.m.winer@gmail.com>
2026-04-19 11:07:46 -0400

    building dev environment

---
+++ flake.nix
@@ -0,0 +1,46 @@
+{
+  description = "flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-25.11";
+  };
+
+  outputs = { self, nixpkgs, ... }: 
+  let 
+    system = "x86_64-linux";
+    pkgs = import nixpkgs { inherit system; };
+  in {
+    devShells.${system}.default = pkgs.mkShell {
+      buildInputs = with pkgs; [
+        nodejs_24
+        pnpm
+      ];
+
+      shellHook = ''
+        export PNPM_HOME="$PWD/pnpm_home"
+        export PATH="$PNPM_HOME:$PATH"
+        pnpm i -g --frozen-lockfile
+
+        code .
+
+        exec fish -C 'function fish_prompt
+          set_color cyan
+          echo -n (whoami)
+          
+          set_color white
+          echo -n "@"
+          
+          set_color blue
+          echo -n "learn-the-fretboard "
+          
+          set_color yellow
+          echo -n (prompt_pwd)
+          
+          set_color white
+          echo "> "
+        end'
+      '';
+    };
+
+  };
+}
